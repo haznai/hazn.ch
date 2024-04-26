c4c4d3b8475839e0f21ec9fb7e4c1de5af3f48f3
hazn <aitakprojects@gmail.com>
2024-04-26 20:31:52 +0200

    fonts and layout

--- _config.ts
+++ _config.ts
@@ -1,11 +1,14 @@
 import lume from "lume/mod.ts";
 import nav from "lume/plugins/nav.ts";
+import lightningCss, { version } from "lume/plugins/lightningcss.ts";
 
 const site = lume({
     src: "./files",
     dest: "./_site",
   }
 );
+
+site.use(lightningCss());
 
 // access to file tree
 site.use(nav());
